16641eb6af97c3339c5e83d77196a9175be12be9
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-17 17:30:43 -0300

    feat: add more filters and componentize table

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,43 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="size-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        239879dsf97we83
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="size-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">
+        Eduardo de Almeida Fernandes
+      </TableCell>
+      <TableCell className="font-medium">R$ 149,99</TableCell>
+      <TableCell className="font-medium">
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 size-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell className="font-medium">
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 size-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
