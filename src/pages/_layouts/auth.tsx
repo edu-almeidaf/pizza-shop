e7152c63633b0d2b49f3ccf4ea10134fed6a01f2
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-17 12:18:11 -0300

    feat: Create sign up page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2">
-      <div className="bg-muted border-foreground/5 text-muted-foreground flex h-full flex-col justify-between border-r p-10">
-        <div className="text-foreground flex items-center gap-3 text-lg">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="size-5" />
           <span className="font-semibold">pizza.shop</span>
         </div>
@@ -15,7 +15,7 @@ export function AuthLayout() {
         </footer>
       </div>
 
-      <div className="flex flex-col items-center justify-center">
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
-import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -20,15 +22,24 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
+    try {
+      console.log(data)
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu email.')
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
