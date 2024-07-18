a39c5c253f4c90b6f21cb36c5a329b99d93c0c55
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-18 20:18:51 -0300

    feat: list orders

---
+++ src/components/order-status.tsx
@@ -0,0 +1,37 @@
+import { twMerge } from 'tailwind-merge'
+
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface OrderStatusProps {
+  status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
+}
+
+const orderColorMap: Record<OrderStatus, string> = {
+  pending: 'bg-slate-400',
+  canceled: 'bg-rose-500',
+  delivered: 'bg-emerald-500',
+  delivering: 'bg-amber-500',
+  processing: 'bg-amber-500',
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span className={twMerge('size-2 rounded-full', orderColorMap[status])} />
+      <span>{orderStatusMap[status]}</span>
+    </div>
+  )
+}
